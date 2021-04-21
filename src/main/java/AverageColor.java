ece470ac2691b602dcb96fc9e63875c2f7431c8b
Juan Diego Serrano <juandserrano@gmail.com>
2021-04-21 10:08:45 -0400

    first commit

---
+++ src/main/java/AverageColor.java
@@ -0,0 +1,65 @@
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Scanner;
+
+public class AverageColor {
+
+    public static Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+    public static BufferedImage screenCapture;
+    public static BufferedImage capture;
+    public static MQTT mqtt = new MQTT();
+
+
+    public static void main(String[] args) throws Exception {
+        boolean quit = false;
+        Scanner input = new Scanner(System.in);
+        while(!quit) {
+            mqtt.message(run());
+
+            Thread.sleep(3000);
+        }
+
+    }
+
+    public static BufferedImage givenMainScreen_whenTakeScreenshot_thenSaveToFile() throws Exception {
+
+        capture = new Robot().createScreenCapture(screenRect);
+        return capture;
+    }
+
+    public static Color averageColor(BufferedImage bi,int x0, int y0, int w, int h) throws Exception{
+        int x1 = x0 + w;
+        int y1 = y0 + h;
+        long sumr = 0, sumg = 0, sumb = 0;
+        for (int x = x0; x < x1; x++) {
+            for (int y = y0; y < y1; y++) {
+                Color pixel = new Color(bi.getRGB(x, y));
+                sumr += pixel.getRed();
+                sumg += pixel.getGreen();
+                sumb += pixel.getBlue();
+            }
+        }
+        int num = w * h;
+
+        int ri = Integer.parseInt(Long.toString(sumr/num));
+        int gi = Integer.parseInt(Long.toString(sumg/num));
+        int bi2 = Integer.parseInt(Long.toString(sumb/num));
+
+        Color averageColor = new Color(ri, gi, bi2);
+        return averageColor;
+    }
+
+    public static String run() throws Exception{
+
+        screenCapture = givenMainScreen_whenTakeScreenshot_thenSaveToFile();
+        int h = screenCapture.getHeight();
+        int w = screenCapture.getWidth();
+        Color average_color = averageColor(screenCapture, 0, 0, w, h);
+        int r = average_color.getRed();
+        int g = average_color.getGreen();
+        int b = average_color.getBlue();
+        String hex = String.format("#%02X%02X%02X", r, g, b);
+        return hex;
+    }
+}
---
+++ src/main/java/MQTT.java
@@ -0,0 +1,39 @@
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MQTT {
+    public void message(String hex) {
+
+        String topic        = "sensors/macbook/monitorcolor";
+        int qos             = 2;
+        String broker       = "tcp://192.168.50.111:1883";
+        String clientId     = "MacBook";
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            System.out.println("Publishing message: "+ hex);
+            MqttMessage message = new MqttMessage(hex.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
+    }
+}
