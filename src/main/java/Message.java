895fa847e6c1182514d8d75511e834fea26c17a1
Juan Diego Serrano <juandserrano@gmail.com>
2021-04-23 14:11:32 -0400

    adding Brightness steps

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -3,10 +3,11 @@ import java.math.BigInteger;
 public class Message {
     private String hexColor;
     private String brightness;
+    private int[] values = {0, 5 , 10 , 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100};
 
     public Message(String hexColor, String brightness) {
         this.hexColor = hexColor;
-        this.brightness = Double.toString(Math.round(Double.parseDouble(brightness)));
+        setBrightness(brightness);
 
     }
 
@@ -22,7 +23,16 @@ public class Message {
         this.hexColor = hexColor;
     }
 
-    public void setAverageChannel(String averageChannel) {
-        this.brightness = averageChannel;
+    public void setBrightness(String brightness) {
+        double doubleBrightness = Double.parseDouble(brightness);
+        double lowestDifference = 101;
+        int lowestDiffValueIndex = 0;
+        for (int i = 0; i < values.length; i++){
+            if (Math.abs(values[i] - doubleBrightness) < lowestDifference){
+                lowestDiffValueIndex = i;
+                lowestDifference = Math.abs(values[i] - doubleBrightness);
+            }
+        }
+        this.brightness = Integer.toString(values[lowestDiffValueIndex]);
     }
 }
